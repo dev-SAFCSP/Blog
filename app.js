dca9cdee462733d2bf99cf47db9c63e1b1a05f19
dev <dev@test.com>
2021-11-19 22:12:24 +0300

    delete a user now is working with delete http verb, thanks to method-override library

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ mongoose.connect('mongodb://localhost:27017/blog',{
     useNewUrlParser:true,
     useUnifiedTopology: true
 });
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 
-//app.use(methodOverride('_method'),{methods:['POST','GET']})
+app.use(methodOverride('_method',{methods:['POST','GET']}))
 app.set('view engine','ejs');
 app.use(layouts);
 
 app.get('/',userController.index);
 app.get('/create',userController.createForm);
 app.post('/create',userController.create);
-app.get('/delete/:id',userController.delete);
+app.delete('/delete/:id',userController.delete);
 app.get('/update/:id/:email', userController.update);
 
 app.listen('3000', ()=> console.log('express listen on port 3000'))
